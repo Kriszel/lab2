eb7544e810f0bac5d52813c58f855a5bf6be5939
KEN <84188493+Kriszel@users.noreply.github.com>
2023-02-13 15:51:52 +0800

    Connected the form to phpmyadmin

--- week9/form_validation.php
+++ week9/form_validation.php
@@ -100,5 +100,34 @@ echo "<br>";
 echo $gender;
 ?>
 
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
+	$servername = "localhost";
+	$username = "webprogss211";
+	$password = "webprogss211";
+	$dbname = "webprogss211db";
+	
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+	}
+	
+	$sql = "INSERT INTO myguests (name, email, website, comments, gender)
+	VALUES ('$name','$email','$website','$comments','$gender')";
+	
+	if ($conn->query($sql) === TRUE) {
+	echo "New record created successfully";
+	} else {
+	echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+	
+	$conn->close();	
+	
+}
+?>
+
 </body>
 </html>
